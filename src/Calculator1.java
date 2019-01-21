5494581423feafe66ebff3935c7e4cfa092b99a6
ibrahimcakmak <ibrcakmak@gmail.com>
2019-01-20 23:19:51 -0500

    local:add divide method

--- src/Calculator1.java
+++ src/Calculator1.java
@@ -11,4 +11,10 @@ public class Calculator1 {
     public static double multiply(double num1, double num2) {
         return num1*num2;
     }
+
+
+    public static double divide(double num1, double num2) {
+        double result= num1/num2;
+        return result;
+    }
 }
