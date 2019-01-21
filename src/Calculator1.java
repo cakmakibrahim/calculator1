d6cf810b79bad40b1f2001721e25260ea449d012
ibrahimcakmak <ibrcakmak@gmail.com>
2019-01-20 20:33:23 -0500

    add subtartc method

--- src/Calculator1.java
+++ src/Calculator1.java
@@ -2,4 +2,8 @@ public class Calculator1 {
     public static double add(double num1, double num2) {
         return num1 + num2;
     }
+
+    public static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
 }
